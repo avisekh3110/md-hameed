c8b261793e999effc88f72c79bb2e5b99603bac4
Mind0Bender <mrcircuit1234@gmail.com>
2021-05-19 18:18:27 +0530

    Better error handling, adminKey setup & accessing data via a route

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ const cors = require("cors");
 
 let allData = [];
 
+const adminKey = "UUO2ZQPTOKTQ33DU";
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+const ISDEV = process.env.NODE_ENV !== "production";
 
 app.use(cors());
 
 app.use("/", express.static("public"));
 
 app.get("/allData", (req, res) => {
-  res.send(allData);
+  if (req.query.adminkey === adminKey) {
+    res.send(allData);
+  } else {
+    res.send("Chal Bhagle yaha se. :P");
+  }
+});
+
+app.get("/throwError", (req, res) => {
+  throw new Error("This is an intentional Error! :D");
 });
 
 let server = app.listen(PORT, () => {
@@ -32,14 +43,24 @@ io.sockets.on("connection", (soc) => {
     console.log(allData);
   });
   soc.on("getAllData", (password) => {
-    if (password === "UUO2ZQPTOKTQ33DU") {
+    if (password === adminKey) {
       soc.emit("gotAllData", allData);
       console.log("Sending data");
     }
   });
   soc.on("cls", (password) => {
-    if (password === "UUO2ZQPTOKTQ33DU") {
+    if (password === adminKey) {
       allData = [];
     }
   });
 });
+
+function errorHandler(err, req, res, next) {
+  console.error(`Err:  ${chalk.redBright(err.message)} \nstack: ${err.stack}`);
+  res.send({
+    msg: ISDEV ? err.message : "There was some internal server Error D:",
+    stack: ISDEV ? err.location : "Contact developer for help",
+  });
+}
+
+app.use(errorHandler);
